32f33a84ae3258dd2c1f4d7e0faa823c96b9d315
Jesse Peterson <jesse@mactechs.com>
2017-03-01 16:56:29 -0800

    Added scalar pointer import tests in sub-struct

--- import_test.go
+++ import_test.go
@@ -4,8 +4,10 @@ import "testing"
 
 func TestSimpleStructImport1(t *testing.T) {
 	type TestSubStruct struct {
-		TestSubString string `kvconfig:"test_sub_string"`
-		TestSubInt    int    `kvconfig:"test_sub_int"`
+		TestSubString    string  `kvconfig:"test_sub_string"`
+		TestSubInt       int     `kvconfig:"test_sub_int"`
+		TestSubPtrString *string `kvconfig:"test_sub_ptr_string"`
+		TestSubPtrInt    *int    `kvconfig:"test_sub_ptr_int"`
 	}
 
 	type TestStruct struct {
@@ -19,12 +21,14 @@ func TestSimpleStructImport1(t *testing.T) {
 	ts := TestStruct{}
 
 	kv := &MapStrStr{
-		"test_string_0":     "test",
-		"test_int_0":        "1",
-		"test_ptr_string_0": "test2",
-		"test_ptr_int_0":    "2",
-		"test_sub_string_0": "test3",
-		"test_sub_int_0":    "3",
+		"test_string_0":         "test",
+		"test_int_0":            "1",
+		"test_ptr_string_0":     "test2",
+		"test_ptr_int_0":        "2",
+		"test_sub_string_0":     "test3",
+		"test_sub_int_0":        "3",
+		"test_sub_ptr_string_0": "test4",
+		"test_sub_ptr_int_0":    "4",
 	}
 
 	Import(kv, &ts)
@@ -61,6 +65,18 @@ func TestSimpleStructImport1(t *testing.T) {
 		}
 		if ts.SubStructs[0].TestSubString != "test3" {
 			t.Errorf("TestStruct.SubStructs[0].TestSubString) = %q; wanted %q", ts.SubStructs[0].TestSubString, "test3")
+		}
+
+		if ts.SubStructs[0].TestSubPtrString == nil {
+			t.Error("TestStruct.SubStructs[0].TestSubPtrString == nil")
+		} else if *(ts.SubStructs[0].TestSubPtrString) != "test4" {
+			t.Errorf("*(TestStruct.SubStructs[0].TestSubPtrString) = %q; wanted %q", *(ts.SubStructs[0].TestSubPtrString), "test4")
+		}
+
+		if ts.SubStructs[0].TestSubPtrInt == nil {
+			t.Error("TestStruct.SubStructs[0].TestSubPtrInt == nil")
+		} else if *(ts.SubStructs[0].TestSubPtrInt) != 4 {
+			t.Errorf("*(TestStruct.SubStructs[0].TestSubPtrInt) = %d; wanted %d", *(ts.SubStructs[0].TestSubPtrInt), 4)
 		}
 	}
 
