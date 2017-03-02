9f1b1aae9f9962f1c3c89ca201dde1bdae3592f7
Jesse Peterson <jesse@mactechs.com>
2017-03-01 16:58:43 -0800

    Added scalar pointer export tests in sub-struct

--- export_test.go
+++ export_test.go
@@ -4,8 +4,10 @@ import "testing"
 
 func TestSimpleStructExport1(t *testing.T) {
 	type TestSubStruct struct {
-		TestSubString string `kvconfig:"test_sub_string"`
-		TestSubInt    int    `kvconfig:"test_sub_int"`
+		TestSubString    string  `kvconfig:"test_sub_string"`
+		TestSubInt       int     `kvconfig:"test_sub_int"`
+		TestSubPtrString *string `kvconfig:"test_sub_ptr_string"`
+		TestSubPtrInt    *int    `kvconfig:"test_sub_ptr_int"`
 	}
 
 	type TestStruct struct {
@@ -19,13 +21,20 @@ func TestSimpleStructExport1(t *testing.T) {
 	testStr := "test2"
 	testInt := 2
 
+	testSubStr := "test4"
+	testSubInt := 4
+
 	ts := TestStruct{
 		TestString:    "test",
 		TestInt:       1,
 		TestPtrString: &testStr,
 		TestPtrInt:    &testInt,
 		SubStructs: []*TestSubStruct{
-			&TestSubStruct{TestSubString: "test3", TestSubInt: 3},
+			&TestSubStruct{
+				TestSubString:    "test3",
+				TestSubInt:       3,
+				TestSubPtrString: &testSubStr,
+				TestSubPtrInt:    &testSubInt},
 		},
 	}
 
@@ -34,12 +43,14 @@ func TestSimpleStructExport1(t *testing.T) {
 	Export(&ts, kv)
 
 	testTable := map[string]string{
-		"test_string_0":     "test",
-		"test_int_0":        "1",
-		"test_ptr_string_0": "test2",
-		"test_ptr_int_0":    "2",
-		"test_sub_string_0": "test3",
-		"test_sub_int_0":    "3",
+		"test_string_0":         "test",
+		"test_int_0":            "1",
+		"test_ptr_string_0":     "test2",
+		"test_ptr_int_0":        "2",
+		"test_sub_string_0":     "test3",
+		"test_sub_int_0":        "3",
+		"test_sub_ptr_string_0": "test4",
+		"test_sub_ptr_int_0":    "4",
 	}
 
 	for k, v := range testTable {
